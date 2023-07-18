067db3d41391630d27739f199d3e7adc280fee24
ElitonNTT <netinho_123_anps@hotmail.com>
2023-07-18 15:51:00 -0300

    Criado função para marcar como executado

---
+++ src/components/ModalExec.tsx
@@ -0,0 +1,35 @@
+import { useSession } from "next-auth/react"
+import { useRouter } from "next/navigation"
+import { useModalOpen } from "@/hooks/useModalOpen"
+
+interface ModalExecProps {
+  id: string,
+}
+
+function ModalExec(props: ModalExecProps) {
+
+  const { isOpen, handleClose } = useModalOpen()
+
+  const router = useRouter()
+  const { data: session } = useSession()
+
+  const handleSubmitExec = async (e: any) => {
+    e.preventDefault()
+    const data = await fetch('/api/tasks', {
+      method: 'PUT',
+      body: JSON.stringify({ id: props.id, executado: session?.user?.name })
+    })
+    const json = await data.json()
+    handleClose()
+    return json
+  }
+
+  return (
+    <div className="absolute left-2 bg-white rounded-md justify-between z-10 flex flex-col p-4 shadow-md gap-2">
+      <h2 className="text-lg font-semibold">Iniciar Tarefa</h2>
+      <button onClick={handleSubmitExec} className='bg-green-400 hover:bg-green-600 hover:text-white rounded-md px-4 py-2'>Confirmar</button>
+    </div >
+  )
+}
+
+export default ModalExec
